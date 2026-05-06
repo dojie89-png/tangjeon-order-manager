8367d28306abefac036a27499aaa87c40d8942b9
Doyoung Yoo <doyoungyoo@local>
2026-05-06 14:34:12 +0900

    첫 업로드

--- build_windows.bat
+++ build_windows.bat
@@ -3,43 +3,23 @@ setlocal
 
 cd /d "%~dp0"
 
-echo [1/4] Installing dependencies...
+echo [1/3] Installing dependencies...
 py -m pip install --upgrade pip
 py -m pip install --upgrade -r requirements.txt
 py -m pip install --upgrade pyinstaller
 py -c "import selenium, sys; print('Python:', sys.executable); print('Selenium:', selenium.__version__)"
 
-echo [2/4] Cleaning previous build files...
+echo [2/3] Cleaning previous build files...
 if exist build rmdir /s /q build
 if exist dist rmdir /s /q dist
-if exist "케이진_탕전주문관리_v12.56.spec" del /q "케이진_탕전주문관리_v12.56.spec"
+if exist "order_export_gui.spec" del /q "order_export_gui.spec"
 
-echo [3/4] Building exe...
-py -m PyInstaller ^
-  --onefile ^
-  --windowed ^
-  --clean ^
-  --name "케이진_탕전주문관리_v12.56" ^
-  --collect-all selenium ^
-  --collect-submodules selenium ^
-  --collect-submodules selenium.webdriver ^
-  --hidden-import selenium.webdriver.chrome.webdriver ^
-  --hidden-import selenium.webdriver.chrome.service ^
-  --hidden-import selenium.webdriver.chrome.options ^
-  --hidden-import selenium.webdriver.chromium.webdriver ^
-  --hidden-import selenium.webdriver.chromium.service ^
-  --hidden-import selenium.webdriver.chromium.options ^
-  --hidden-import selenium.webdriver.common.service ^
-  --hidden-import selenium.webdriver.common.driver_finder ^
-  --hidden-import selenium.webdriver.common.selenium_manager ^
-  --hidden-import selenium.webdriver.remote.webdriver ^
-  --hidden-import selenium.webdriver.remote.remote_connection ^
-  --hidden-import selenium.webdriver.support.expected_conditions ^
-  order_export_gui.py
+echo [3/3] Building exe...
+py -m PyInstaller --onefile --windowed --collect-all selenium --name order_export_gui order_export_gui.py
 
-echo [4/4] Done.
+echo Done.
 echo.
 echo EXE path:
-echo %CD%\dist\케이진_탕전주문관리_v12.56.exe
+echo %CD%\dist\order_export_gui.exe
 echo.
 pause
